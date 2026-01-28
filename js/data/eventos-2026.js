a86d6735a89480a207656208a607cb0a0372b448
Wolney <73698976+GabrielWolney@users.noreply.github.com>
2026-01-28 00:39:30 -0300

    fix: datas calendario

--- js/data/eventos-2026.js
+++ js/data/eventos-2026.js
@@ -1,88 +1,78 @@
 // Dados extraídos do Calendário Ágape 2026
 export const eventos2026 = {
-    // JANEIRO
-    "15-1": { titulo: "Retorno do Ágape", tipo: "evento" },
-    "23-1": { titulo: "Workshop 1", tipo: "formacao" },
-    "24-1": { titulo: "Workshop 1", tipo: "formacao" },
+    "18-1": { titulo: "Retorno do Ágape", tipo: "evento" },
     "25-1": { titulo: "Workshop 1", tipo: "formacao" },
-
-    // FEVEREIRO
     "1-2": { titulo: "Workshop 2", tipo: "formacao" },
-    "7-2": { titulo: "Workshop 3", tipo: "formacao" },
+    "4-2": { titulo: "Intercessão pré-encontro", tipo: "oracao" },
     "8-2": { titulo: "Workshop 3", tipo: "formacao" },
+    "9-2": { titulo: "Intercessão pré-encontro", tipo: "oracao" },
     "15-2": { titulo: "Carnaval", tipo: "feriado" },
-    "21-2": { titulo: "Noite de Tortas", tipo: "festivo" },
-    "22-2": { titulo: "Mutirão de Mensagens", tipo: "acao" },
-
-    // MARÇO
-    "1-3": { titulo: "Passagem do Encontro", tipo: "reuniao" },
-    "7-3": { titulo: "Missa de Envio", tipo: "liturgia" },
+    "21-2": { titulo: "Noite de tortas", tipo: "festivo" },
+    "22-2": { titulo: "Mutirão de mensagens", tipo: "acao" },
+    "23-2": { titulo: "Intercessão pré-encontro", tipo: "oracao" },
+    "1-3": { titulo: "Passagem do encontro", tipo: "reuniao" },
+    "4-3": { titulo: "Missa de envio", tipo: "liturgia" },
+    "7-3": { titulo: "Encontro Ágape", tipo: "evento" },
     "8-3": { titulo: "Encontro Ágape", tipo: "evento" },
-    "15-3": { titulo: "Tema: Filho Pródigo", tipo: "formacao" },
-    "21-3": { titulo: "1ª Vivência de Cursistas", tipo: "vivencia" },
-    "22-3": { titulo: "1ª Formação de Servos", tipo: "formacao" },
-    "29-3": { titulo: "Relação com Deus (Caça ao Tesouro)", tipo: "dinamica" },
-
-    // ABRIL
+    "15-3": { titulo: "Tema: Filho pródigo", tipo: "formacao" },
+    "21-3": { titulo: "1ª vivência de cursistas", tipo: "vivencia" },
+    "22-3": { titulo: "1ª formação de servos", tipo: "formacao" },
+    "29-3": { titulo: "Relação com Deus (caça ao tesouro)", tipo: "dinamica" },
     "5-4": { titulo: "Páscoa do Senhor", tipo: "solenidade" },
-    "12-4": { titulo: "Ágape: Viver a Caridade", tipo: "acao" },
+    "12-4": { titulo: "Ágape, viver a caridade", tipo: "acao" },
     "19-4": { titulo: "Tema: Deus Pai", tipo: "formacao" },
-    "25-4": { titulo: "2ª Vivência de Cursistas", tipo: "vivencia" },
-    "26-4": { titulo: "2ª Formação de Servos", tipo: "formacao" },
-
-    // MAIO
+    "25-4": { titulo: "2ª vivência de cursistas", tipo: "vivencia" },
+    "26-4": { titulo: "2ª formação de servos", tipo: "formacao" },
+    "27-4": { titulo: "Intercessão", tipo: "oracao" },
     "3-5": { titulo: "Tema: Deus Filho", tipo: "formacao" },
-    "10-5": { titulo: "Dia das Mães", tipo: "festivo" },
+    "10-5": { titulo: "Dia das mães", tipo: "festivo" },
     "17-5": { titulo: "Deus Espírito Santo / Confirmação", tipo: "formacao" },
-    "23-5": { titulo: "3ª Vivência / Vigília Pentecostes", tipo: "vivencia" },
-    "24-5": { titulo: "3ª Formação de Servos", tipo: "formacao" },
+    "23-5": { titulo: "3ª vivência / Pentecostes", tipo: "vivencia" },
+    "24-5": { titulo: "3ª formação de servos", tipo: "formacao" },
+    "25-5": { titulo: "Intercessão", tipo: "oracao" },
+    "29-5": { titulo: "Festa Junina", tipo: "festivo" },
     "30-5": { titulo: "Festa Junina", tipo: "festivo" },
     "31-5": { titulo: "Festa Junina", tipo: "festivo" },
-
-    // JUNHO
     "7-6": { titulo: "Tema: Maria", tipo: "formacao" },
-    "14-6": { titulo: "Tema: Fim do Homem", tipo: "formacao" },
+    "14-6": { titulo: "Tema: Fim do homem", tipo: "formacao" },
     "21-6": { titulo: "Tema: Novíssimos", tipo: "formacao" },
-    "27-6": { titulo: "4ª Vivência de Cursistas", tipo: "vivencia" },
-    "28-6": { titulo: "4ª Formação de Servos", tipo: "formacao" },
-
-    // JULHO
-    "5-7": { titulo: "Tema: Ser Igreja", tipo: "formacao" },
-    "12-7": { titulo: "Remissão dos Pecados", tipo: "formacao" },
-    "18-7": { titulo: "Confra Fim de Semestre", tipo: "festivo" },
+    "27-6": { titulo: "4ª vivência de cursistas", tipo: "vivencia" },
+    "28-6": { titulo: "4ª formação de servos", tipo: "formacao" },
+    "29-6": { titulo: "Intercessão", tipo: "oracao" },
+    "5-7": { titulo: "Tema: Ser igreja", tipo: "formacao" },
+    "12-7": { titulo: "Remissão dos pecados", tipo: "formacao" },
+    "18-7": { titulo: "Confra fim do semestre", tipo: "festivo" },
     "19-7": { titulo: "Início das Férias", tipo: "feriado" },
-
-    // AGOSTO
-    "9-8": { titulo: "Luau / Dia dos Pais", tipo: "festivo" },
-    "16-8": { titulo: "Relacionamento com Deus", tipo: "formacao" },
-    "22-8": { titulo: "5ª Vivência de Cursistas", tipo: "vivencia" },
-    "23-8": { titulo: "5ª Formação de Servos", tipo: "formacao" },
-    "30-8": { titulo: "Relacionamento Consigo", tipo: "formacao" },
-
-    // SETEMBRO
-    "6-9": { titulo: "Relacionamento com o Próximo", tipo: "formacao" },
-    "10-9": { titulo: "Círio", tipo: "evento" },
-    "13-9": { titulo: "Círio", tipo: "evento" },
-    "20-9": { titulo: "Relacionamento Familiar", tipo: "formacao" },
-    "26-9": { titulo: "6ª Vivência de Cursistas", tipo: "vivencia" },
-    "27-9": { titulo: "6ª Formação de Servos", tipo: "formacao" },
-
-    // OUTUBRO
+    "26-7": { titulo: "Férias", tipo: "feriado" },
+    "2-8": { titulo: "Férias", tipo: "feriado" },
+    "8-8": { titulo: "Luau Ágape", tipo: "festivo" },
+    "9-8": { titulo: "Dia dos pais", tipo: "festivo" },
+    "16-8": { titulo: "Tema: Relacionamento com Deus", tipo: "formacao" },
+    "22-8": { titulo: "5ª vivência de cursistas", tipo: "vivencia" },
+    "23-8": { titulo: "5ª formação de servos", tipo: "formacao" },
+    "25-8": { titulo: "Intercessão", tipo: "oracao" },
+    "30-8": { titulo: "Tema: Relacionamento consigo", tipo: "formacao" },
+    "6-9": { titulo: "Tema: Relacionamento com o próximo", tipo: "formacao" },
+    "10-9": { titulo: "Círio de Nazaré", tipo: "evento" },
+    "11-9": { titulo: "Círio de Nazaré", tipo: "evento" },
+    "12-9": { titulo: "Círio de Nazaré", tipo: "evento" },
+    "13-9": { titulo: "Círio de Nazaré", tipo: "evento" },
+    "20-9": { titulo: "Tema: Relacionamento familiar", tipo: "formacao" },
+    "26-9": { titulo: "6ª vivência de cursistas", tipo: "vivencia" },
+    "27-9": { titulo: "6ª formação de servos", tipo: "formacao" },
+    "29-9": { titulo: "Intercessão", tipo: "oracao" },
     "4-10": { titulo: "Tema: Castidade", tipo: "formacao" },
-    "11-10": { titulo: "Tema: Drogas e Vícios", tipo: "formacao" },
+    "11-10": { titulo: "Tema: Drogas/Vícios", tipo: "formacao" },
     "18-10": { titulo: "Tema: Vocação", tipo: "formacao" },
-    "24-10": { titulo: "7ª Vivência de Cursistas", tipo: "vivencia" },
+    "24-10": { titulo: "7ª vivência de cursistas", tipo: "vivencia" },
     "25-10": { titulo: "Tema: Redes Sociais", tipo: "formacao" },
-
-    // NOVEMBRO
-    "1-11": { titulo: "Chamado à Santidade", tipo: "formacao" },
+    "27-10": { titulo: "Intercessão", tipo: "oracao" },
+    "1-11": { titulo: "Tema: Chamado a Santidade", tipo: "formacao" },
     "7-11": { titulo: "Adoração ao Santíssimo", tipo: "oracao" },
-    "8-11": { titulo: "Viver em Comunidade", tipo: "formacao" },
-    "15-11": { titulo: "Missa Parte a Parte", tipo: "liturgia" },
+    "8-11": { titulo: "Tema: Viver em comunidade", tipo: "formacao" },
+    "15-11": { titulo: "Missa parte a parte", tipo: "liturgia" },
     "22-11": { titulo: "Tema: Querigma", tipo: "formacao" },
     "29-11": { titulo: "Projeto Ágape", tipo: "acao" },
-
-    // DEZEMBRO
-    "6-12": { titulo: "Retiro de Servos", tipo: "retiro" },
-    "13-12": { titulo: "Confra Fim de Ano", tipo: "festivo" }
+    "6-12": { titulo: "Retiro de servos", tipo: "retiro" },
+    "13-12": { titulo: "Confra fim de ano", tipo: "festivo" }
 };
